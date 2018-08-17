5e2de1c42d66ad1e5df5fc29ed05b766e6bf1fc3
Elton Lin <eltonlin1998@gmail.com>
2018-08-17 19:18:12 +0300

    Offline whiteboard functional

--- src/components/chat/Whiteboard.vue
+++ src/components/chat/Whiteboard.vue
@@ -7,12 +7,58 @@
 
 <script>
 import paper from 'paper'
+import { version } from 'moment';
+
+
 export default {
   created () {
     paper.install(window)
   },
+  data () {
+    return {
+      path: null 
+    }
+  },
   mounted () {
     paper.setup('whiteboard')
+    var tool = new Tool()
+    console.log(`tool = ${tool}`)
+    tool.onMouseDown = event => {
+      this.path = new Path()
+      this.path.strokeColor = 'black'
+    }
+
+    tool.onMouseDrag = event => {
+      this.path.add(event.point)
+    }
+
+    tool.onMouseUp = event => {
+      this.path.add(event.point)
+	    this.path.simplify()
+    }
+
+  },
+  data () {
+    return {
+      myPath: null 
+    }
+  },
+  methods: {
+    onMouseDown(event) {
+      myPath = new Path();
+      myPath.strokeColor = 'black';
+    },
+    onMouseDrag(event) {
+      myPath.add(event.point);
+    },
+    onMouseUp(event) {
+      var myCircle = new Path.Circle({
+        center: event.point,
+        radius: 10
+      });
+      myCircle.strokeColor = 'black';
+      myCircle.fillColor = 'white';
+    }
   }
 }
 </script>
@@ -21,6 +67,6 @@ export default {
   canvas {
     height: 100%;
     width: 100%;
-    background: lightblue;
+    background: white;
   }
 </style>
