a86c75438a6af68d96009804f1c47581b5244f0f
Elton Lin <eltonlin1998@gmail.com>
2018-08-17 20:54:12 +0300

    Able to render white board with pre-drawn content

--- src/components/chat/Whiteboard.vue
+++ src/components/chat/Whiteboard.vue
@@ -16,49 +16,61 @@ export default {
   },
   data () {
     return {
-      path: null 
+      path: null,
+      allPaths: [],
+      example: { 
+        point: { x: 576, y: 92.19034 }, 
+        handleOut: { x: -14.67857, y: 1.00595 } 
+      }
     }
   },
   mounted () {
     paper.setup('whiteboard')
     var tool = new Tool()
-    console.log(`tool = ${tool}`)
     tool.onMouseDown = event => {
       this.path = new Path()
       this.path.strokeColor = 'black'
     }
-
     tool.onMouseDrag = event => {
       this.path.add(event.point)
+      console.log(`path = ${this.path.toString()}`)
     }
-
     tool.onMouseUp = event => {
       this.path.add(event.point)
-	    this.path.simplify()
+      console.log('event.point =', event.point)
+      this.path.simplify()
+      this.path.smooth()
+      const segments = this.path.getSegments()
+      console.log(`segments = ${segments}`)
+      console.log(`EXAMPLE = ${this.example}`)
+      this.allPaths.push(this.path)
+      this.path = null 
+      console.log(`paper = ${paper}`)
     }
+    // see if you can initialize some drawings on the Canvas 
+    // var examplePath = new Path()
+    // examplePath._segments = { point: { x: 576, y: 92.19034 }, handleOut: { x: -14.67857, y: 1.00595 } }
+    // console.log('successfully set example path')
+    // const examplePathSegments = examplePath.getSegments()
+    // console.log('examplePathSegments =', examplePathSegments)
+    // paper.view.update()
+    // console.log(`paper.view = ${paper.view}`)
+    var path = new Path()
+    path.strokeColor = 'black'
+    var start = new Point(100, 100)
+    path.moveTo(start)
+    path.lineTo(start.add([ 200, -50 ]))
+    view.draw()
+    var anotherPath = new Path()
+    anotherPath.strokeColor = 'yellow'
+    var s2 = new Point(50, 50)
+    anotherPath.moveTo(s2)
+    // see if using "add()" will automatically trigger an update event
+    // anotherPath.lineTo(s2.add([200, 100]))
+    anotherPath.add(new Point(150, 150))
+    // add triggers a redraw
 
-  },
-  data () {
-    return {
-      myPath: null 
-    }
-  },
-  methods: {
-    onMouseDown(event) {
-      myPath = new Path();
-      myPath.strokeColor = 'black';
-    },
-    onMouseDrag(event) {
-      myPath.add(event.point);
-    },
-    onMouseUp(event) {
-      var myCircle = new Path.Circle({
-        center: event.point,
-        radius: 10
-      });
-      myCircle.strokeColor = 'black';
-      myCircle.fillColor = 'white';
-    }
+    
   }
 }
 </script>
