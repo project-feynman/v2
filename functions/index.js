ac321b61c34e8734b3fa762f74892a61d8ce5808
hubierti3 <hubewasi@gmail.com>
2018-08-17 13:48:15 +0200

    Working on push notifications

--- functions/index.js
+++ functions/index.js
@@ -91,6 +91,7 @@ exports.notificationOnNewMessage = functions.firestore.document('/chatRooms/{roo
 	const participants = change.after.data().participants
 	const message = messages[messages.length - 1]
 	const senderName = message.author
+	//return firestore.doc('/users/' + participants[0]).onSnapshot(async snapshot => {
 	firestore.doc('/users/' + participants[0]).get().then(async snapshot => {
 		console.log(snapshot.data())
 		var receiverToken = snapshot.data().token
@@ -100,10 +101,11 @@ exports.notificationOnNewMessage = functions.firestore.document('/chatRooms/{roo
 			const receiverDoc = await firestore.doc('/users/' + participants[1]).get()
 			receiverToken = receiverDoc.data().token
 		}
+		console.log(receiverToken)
 		const payload = {
 			notification : {
 				title: senderName + ' sent you a message...',
-				body: message.contents,
+				body: message.content,
 				clickAction: 'feynman-village.firebaseapp.com'
 			},
 			to: receiverToken
@@ -119,7 +121,9 @@ exports.notificationOnNewMessage = functions.firestore.document('/chatRooms/{roo
 			headers
 		})
 		.post('https://fcm.googleapis.com/fcm/send', payload)
-		.then(response => console.log(response))
+		.then(response => {
+			console.log(response.results)
+		})
 		.catch(error => console.log(error))
 	})
 })
