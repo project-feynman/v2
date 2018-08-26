53e71f91befcf7735f179cc45f6a022aa55c2efe
Elton Lin <eltonlin1998@gmail.com>
2018-08-26 22:12:23 +0100

    Able to fetch user's conversations

--- src/components/profile/MyConvos.vue
+++ src/components/profile/MyConvos.vue
@@ -3,6 +3,8 @@
 </template>
 
 <script>
+import db from '@/firebase/init.js'
+
 export default {
   computed: {
     user () {
@@ -13,6 +15,17 @@ export default {
     if (this.user != 'undetermined' && this.user != null) {
       console.log('app was already opened')
       console.log(`convos = ${JSON.stringify(this.user.conversations)}`)
+      const convos = this.user.conversations
+      convos.forEach(async convo => {
+        // get document from firestore 
+        const ref = db.collection('conversations').doc(convo)
+        const doc = await ref.get() 
+        if (doc.exists) {
+          console.log(`doc data = ${JSON.stringify(doc.data())}`)
+          // display scrollable message 
+          // display whiteboard 
+        }
+      })
     } else {
       console.log('user directly visited this page')
     }
