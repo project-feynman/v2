7980b6dfdf34dde3f17112d471644965e2e16817
Elton Lin <eltonlin1998@gmail.com>
2018-09-07 21:43:55 -0400

    Rename MySubjects to Dashboard

--- src/router.js
+++ src/router.js
@@ -7,13 +7,12 @@ Vue.use(Router)
 const Subject = () => import('@/views/Subject.vue')
 const Pset = () => import('@/views/Pset.vue')
 const Login = () => import('@/views/Login.vue')
-const MySubjects = () => import('@/views/MySubjects.vue')
+const Dashboard = () => import('@/views/Dashboard.vue')
 const Question = () => import('@/views/Question.vue')
 const Profile = () => import('@/views/Profile.vue')
 const Conversation = () => import('@/views/Conversation.vue')
 const Chat = () => import('@/views/Chat.vue')
 const AllStudyGroups = () => import('@/views/AllStudyGroups.vue')
-const Dashboard = () => import('@/views/Dashboard.vue')
 const AllSubjects = () => import('@/views/AllSubjects.vue')
 
 export default new Router({
@@ -25,12 +24,8 @@ export default new Router({
       component: Login
     },
     {
-      path: '/dashboard',
-      component: Dashboard
-    },
-    {
       path: '/subjects',
-      component: MySubjects
+      component: Dashboard
     },
     {
       path: '/add-classes',
