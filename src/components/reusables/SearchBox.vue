618d4cf690f46220803a3c9acca068222c887c3b
Elton Lin <eltonlin1998@gmail.com>
2018-09-13 18:06:03 -0400

    Use a magnifying glass the SearchBox icon

--- src/components/reusables/SearchBox.vue
+++ src/components/reusables/SearchBox.vue
@@ -3,7 +3,7 @@
     <div class="col s12">
       <div class="row">
         <div class="input-field col s12">
-          <i class="material-icons prefix">textsms</i>
+          <i class="material-icons prefix">search</i>
           <input type="text" id="autocomplete-input" class="autocomplete">
           <label for="autocomplete-input">{{ label }}</label>
         </div>
